ea85fbc3f921340726a79fd5bc8b4198d543d10a
Student <example@example.com>
2019-07-08 13:06:55 -0700

    added images, starting functionality

--- src/flappy-bird.js
+++ src/flappy-bird.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import './css/flappy-bird.css';
 
 
 
@@ -9,15 +9,32 @@ $(document).ready(function () {
   var cvs = document.getElementById('flappyBird');
   var ctx = cvs.getContext('2d');
   var kirby = new Image();
-  kirby.src = "./assets/flappy-bird-img/kirby.gif";
+  var bg = new Image();
+  var pipeNorth = new Image();
+  var pipeSouth = new Image();
+  kirby.src = "./../assets/images/kirby.gif";
+  bg.src = "./../assets/images/bg.png";
+  pipeNorth.src = "./../assets/images/pipeNorth.png";
+  pipeSouth.src = "./../assets/images/pipeSouth.png";
+
   // var audioName = new Audio();
   // audioName.src = "audio/audio.png";
 
-ctx.drawImage(kirby,100,150,50,50);
+On keyDown Event : bY -=20;
 
+function draw() {
+  ctx.drawImage(bg,0,-200);
+  ctx.drawImage(kirby,100,150,50,50);
+  ctx.drawImage(pipeNorth,80,0,50,120);
+  ctx.drawImage(pipeSouth,pX,pY+Const);
+  ctx.drawImage(kirby,bX,bY);
 
+  by += gravity;
+  requestAnimationFrame(draw);
 
+}
 
+draw();
 
 
 
